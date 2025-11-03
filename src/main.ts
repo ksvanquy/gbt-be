4c637b8d921e81d4e456f40b1c96cd4ee6bfa3ae
ksvanquy <ksvanquy@gmail.com>
2025-11-03 14:28:07 +0700

    main

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors();
+
+  // app.enableCors({
+  //   origin: ['http://localhost:3000', 'https://example.com'], // danh sách domain được phép
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // các method được phép
+  //   allowedHeaders: 'Content-Type, Accept, Authorization', // headers được phép
+  //   credentials: true, // nếu cần gửi cookie
+  // });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
